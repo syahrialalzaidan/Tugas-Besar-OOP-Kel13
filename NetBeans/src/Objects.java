6cc68a6122767e6a45d8420818c14be1cd1eb509
syahrialalzaidan <16521445@mahasiswa.itb.ac.id>
2023-04-28 21:14:59 +0700

    commit room

--- NetBeans/src/Objects.java
+++ NetBeans/src/Objects.java
@@ -2,19 +2,26 @@ import java.util.HashMap;
 
 public abstract class Objects {
     private String name;
+<<<<<<< Updated upstream
     private int amount;
     private Inventory inventory;
+=======
+>>>>>>> Stashed changes
 
     public Objects(String name, Inventory inventory) {
         this.name = name;
+<<<<<<< Updated upstream
         this.inventory = inventory;
         // inventory.addItems(name);
         // amount = inventory.getItems().get(name);
+=======
+>>>>>>> Stashed changes
     }
 
     public String getName() {
         return name;
     }
+<<<<<<< Updated upstream
 
     public int getAmount(){
         return amount;
@@ -23,4 +30,6 @@ public abstract class Objects {
     public HashMap<String, Integer> getInventory() {
         return inventory.getInventory();
     }
+=======
+>>>>>>> Stashed changes
 }

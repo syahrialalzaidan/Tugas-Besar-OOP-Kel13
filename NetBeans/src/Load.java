56bf3ccb71bfbd964bd2212b7d0268573cf1fd33
Riandra4trend <18221077@std.stei.itb.ac.id>
2023-05-05 20:38:57 +0700

    save & load new

--- NetBeans/src/Load.java
+++ NetBeans/src/Load.java
@@ -6,10 +6,11 @@ import com.google.gson.Gson;
 
 public class Load {
     public static Sim load(String path) {
+        String pathreal = String.format(path+ ".json");
         Sim world = null;
         try {
             Gson gson = new Gson();
-            Reader reader = Files.newBufferedReader(Paths.get(path));
+            Reader reader = Files.newBufferedReader(Paths.get(pathreal));
             world = gson.fromJson(reader, Sim.class);
         
             System.out.println("Data loaded from " + path + " successfully.");

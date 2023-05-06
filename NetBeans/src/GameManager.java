aa69e7d5a32d88a4619b8db5d5cea1ce595c1026
Gabrielpjt <gabrielpanjaitan46@gmail.com>
2023-05-06 19:25:15 +0700

    Buat Main

--- NetBeans/src/GameManager.java
+++ NetBeans/src/GameManager.java
@@ -128,7 +128,7 @@ public class GameManager {
 
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void run() throws Exception {
         Welcome();
 
         Scanner input = new Scanner(System.in);
